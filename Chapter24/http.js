56aabf3a2862060810c85405d1cc24ba5bb35bac
RandyQin <randyqin@haizol.com>
2020-12-15 17:30:50 +0800

    网络请求2

--- Chapter24/http.js
+++ Chapter24/http.js
@@ -2,9 +2,9 @@ const http = require('http')
 const fs = require('fs')
 
 const server = http.createServer((req, res) => {
-  const {method, url} = req
+  const {method, url, headers } = req
   if(method == 'GET' && url == '/') {
-    fs.readFile('./index.html', (err, data) => {
+    fs.readFile('./Fetch.html', (err, data) => {
       res.setHeader('Content-Type', 'text/html')
       res.end(data)
     })
@@ -12,7 +12,7 @@ const server = http.createServer((req, res) => {
     res.setHeader('Access-Control-Allow-Origin', '*')
     res.setHeader("Content-Type", "application/json");
     res.end(JSON.stringify([{ name: "tom", age: 20 }]));
-  }
+  } 
 })
 server.listen(3000, () => {
   console.log(`服务器运行在 http://localhost:3000/`)
